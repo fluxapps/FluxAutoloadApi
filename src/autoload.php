59e59307945acbdd1284be1f2f7d809de1a662dc
fluxfw <fluxfw@fluxlabs.ch>
2022-03-11 11:24:13 +0100

    fix¨

--- src/autoload.php
+++ src/autoload.php
@@ -5,12 +5,19 @@ namespace FluxAutoloadApi;
 require_once __DIR__ . "/Autoload/Autoload.php";
 require_once __DIR__ . "/Adapter/Autoload/FileAutoload.php";
 require_once __DIR__ . "/Adapter/Autoload/Psr4Autoload.php";
-require_once __DIR__ . "/../libs/polyfill-php80/Php80.php";
 
 use FluxAutoloadApi\Adapter\Autoload\ComposerAutoload;
 use FluxAutoloadApi\Adapter\Autoload\Psr4Autoload;
 use FluxAutoloadApi\Adapter\Checker\PhpExtChecker;
 use FluxAutoloadApi\Adapter\Checker\PhpVersionChecker;
+use Symfony\Polyfill\Php80\Php80;
+use Symfony\Polyfill\Php81\Php81;
+
+$polyfill_php80_autoload = false;
+if (!class_exists(Php80::class)) {
+    $polyfill_php80_autoload = true;
+    require_once __DIR__ . "/../libs/polyfill-php80/Php80.php";
+}
 
 Psr4Autoload::new(
     [
@@ -34,11 +41,17 @@ PhpExtChecker::new(
         __NAMESPACE__
     );
 
-ComposerAutoload::new(
-    __DIR__ . "/../libs/polyfill-php80"
-)
-    ->autoload();
-ComposerAutoload::new(
-    __DIR__ . "/../libs/polyfill-php81"
-)
-    ->autoload();
+if ($polyfill_php80_autoload) {
+    ComposerAutoload::new(
+        __DIR__ . "/../libs/polyfill-php80"
+    )
+        ->autoload();
+}
+unset($polyfill_php80_autoload);
+
+if (!class_exists(Php81::class)) {
+    ComposerAutoload::new(
+        __DIR__ . "/../libs/polyfill-php81"
+    )
+        ->autoload();
+}
